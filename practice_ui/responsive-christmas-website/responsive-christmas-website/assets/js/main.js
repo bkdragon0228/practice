76561716a08ca5383d54edd2c291d493c741f83c
bkdragon0228 <rlaqjarb2067@naver.com>
2022-12-22 21:30:27 +0900

    feat : about-animation, scroll-header-background

--- practice_ui/responsive-christmas-website/responsive-christmas-website/assets/js/main.js
+++ practice_ui/responsive-christmas-website/responsive-christmas-website/assets/js/main.js
@@ -33,6 +33,15 @@ navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+  this.scrollY >= 50
+    ? header.classList.add('bg-header')
+    : header.classList.remove('bg-header');
+};
+
+window.addEventListener('scroll', scrollHeader);
+
 /*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
